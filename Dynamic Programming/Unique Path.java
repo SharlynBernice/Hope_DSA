10fbdf4a575151dee0a31fc50ae51cd6041ddcfd
SharlynBernice <sharlyn10e@gmail.com>
2025-12-08 11:21:05 +0530

    Update Unique Path.java

--- Dynamic Programming/Unique Path.java
+++ Dynamic Programming/Unique Path.java
@@ -23,7 +23,8 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 
 CODE:
 
-
+METHOD 1: MEMOIZATION
+  
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m+1][n+1];
@@ -44,5 +45,26 @@ class Solution {
         } 
         dp[m][n]=helper(m-1,n,dp)+helper(m,n-1,dp);
         return dp[m][n];
+    }
+}
+
+
+METHOD 2: TABULATION
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=1;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j]=1;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
