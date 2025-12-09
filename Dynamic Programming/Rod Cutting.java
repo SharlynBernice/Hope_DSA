45eb13ef5f77c215487197f0b2e82011a68f1e02
SharlynBernice <sharlyn10e@gmail.com>
2025-12-09 21:49:12 +0530

    Update Rod Cutting.java

--- Dynamic Programming/Rod Cutting.java
+++ Dynamic Programming/Rod Cutting.java
@@ -13,6 +13,8 @@ EXPLANATION:
 
 CODE:
 
+METHOD 1: (NORMAL METHOD)
+    
 import java.util.*;
 class Main {
     public static void main(String[] args) {
@@ -40,3 +42,37 @@ class Main {
         System.out.print(max);
     }
 }
+
+
+
+METHOD 2: (USING 2D DP ARRAY LOGIC)
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+
+        int n = z.nextInt();
+        int[] price = new int[n];
+
+        for(int i=0;i<n;i++){
+            price[i] = z.nextInt();
+        }
+        int[][] dp = new int[n+1][n+1];
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+
+                if(i <= j){
+                    dp[i][j] = Math.max(dp[i-1][j],price[i-1] + dp[i][j - i]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+        System.out.println(dp[n][n]); 
+    }
+}
+
